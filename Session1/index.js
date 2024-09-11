06b9c80d92c5be956712e92ab0d57530473f487e
Mohammed Faizan Ghani <mohammedfaizanghani78@gmail.com>
2024-09-11 07:25:29 +0530

    currencies url spliting and data fetching

--- Session1/index.js
+++ Session1/index.js
@@ -1,22 +1,37 @@
 const { time } = require("console");
+const currencies = require("./Currencies.json")
 const http = require("http")
 
 
 const PORT = 8082;
 
 const server = http.createServer((request, response) => {
-    console.log("Server is Created");
-    const date =  new Date().toLocaleDateString()
-    const time = new Date().toLocaleTimeString()
-    const serverinfo = {
-        name : "Mohammed Faizan Ghani",
-        version : 5.0,
-        date,
-        time
+
+    const {url} = request;
+    const splitUrl = url.split("/");
+    // console.log(splitUrl);
+    if(request.url === "/"){
+        response.writeHead(200,{"content-type" : "text/html"});
+        response.write("<h1>Currency Database<h1>")
+    }else if(splitUrl[1] === "currencies"){
+        if(splitUrl[2]){
+            const symbol = splitUrl[2];
+            const symbolData = currencies.data.find(c => c.id === symbol.toUpperCase());
+            if(symbolData){
+                response.writeHead(200,{"content-type" : "application/json"});
+                response.write(JSON.stringify(symbolData));
+            }else{
+                response.writeHead(404);
+            }
+        }else{
+            response.writeHead(200,{"content-type" : "application/json"});
+            response.write(JSON.stringify(currencies))
+        }
+    }else{
+        response.writeHead(404)
     }
-    response.writeHead(200,{"Content-type" : "application/json"})
-    response.write(JSON.stringify(serverinfo))
-    response.end()
+
+     response.end();
 })
 
 server.listen(PORT,()=>{
